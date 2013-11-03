3157086eded592b2d00c581651b5ab20663a5832
jiahonglin <jiahonglin17@gmail.com>
2013-11-03 21:28:50 +0800

    support write text file

--- hello.c
+++ hello.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <inttypes.h>
 
-#define READ_COUNTER_ADDR 0x40050000
 
-int32_t *read_counter = (int32_t *) READ_COUNTER_ADDR;
+#define FILE_PATH "log.txt"
+#define MAX 100
+
+void write_file(FILE *fp, char str[MAX]);
+
 int main(void)
 {
-	printf("This is a test program for QEMU counter device\n");
-	printf("See http://github.com/krasin/qemu-counter for more details\n\n");
-	printf("Let's check if the Read Counter device presented\n");
-	for (int i = 0; i < 10; i++) {
-		printf("The device has been accessed for %"PRId32" times!\n", *read_counter);
-	}
-	int32_t now = *read_counter;
-	if (now == 0) {
-		printf("ERROR - No Read Counter detected\n");
-	}
-	else if (now == 11) {
-		printf("OK - Read Counter works as intended\n");
-	}
-	else {
-		printf("ERROR - Something is wrong with Read Counter\n");
-	}
+	FILE *fp;
+	char file_str[MAX];
+	fp = fopen(FILE_PATH,"r");
+	printf("Write log.txt\n");
+	printf("****************\n");
+	fgets(file_str,MAX,stdin);
+	//printf("%s",file_str);
+	write_file(fp,file_str);
 	return 0;
 }
+
+void write_file(FILE *fp,char str[MAX]){
+	if(fp = fopen(FILE_PATH,"a")){
+		fprintf(fp,"%s",str);
+	}
+	fclose(fp);
+
+}
+
+
